f77e8a83d75b8a55174ac74c11a641ba48a03bc0
riad <maksusbinislam@gmail.com>
2023-09-09 22:09:27 +0600

    add

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from .models import Post, Category , Comment
+from django.utils.html import format_html
+from .models import Post, Category, Comment
 
-admin.site.register(Post)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'email', 'content', 'created_at')
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'date_posted', 'display_image')
+
+    def display_image(self, obj):
+        return format_html('<img src="{}" style="max-height: 100px; max-width: 100px;"/>'.format(obj.image.url))
+
+    display_image.allow_tags = True
+    display_image.short_description = 'Image Preview'
+
+    inlines = [CommentInline]
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
-admin.site.register(Comment)
-
-
+admin.site.register(Comment, CommentAdmin)

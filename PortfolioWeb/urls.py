ad6e98f4a5f4d3851674e3fff2d2d7374705498d
riad <maksusbinislam@gmail.com>
2023-09-10 22:21:12 +0600

    add

--- PortfolioWeb/urls.py
+++ PortfolioWeb/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('gallery.urls')),
     path('blog/', include('blog.urls')),
     path('contact/', include('contact.urls')),
